c62af7406be7c1c2d8523d85694412aa13d971dd
Arun Prasad <akprasad@stanford.edu>
2013-10-09 15:57:52 -0700

    Add dhatupatha.py

---
+++ vyakarana/dhatupatha.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+    vyakarana.dhatupatha
+    ~~~~~~~~~~~~~~~~~~~~
+
+    Coordinates access to the Dhatupatha.
+
+    :license: MIT and BSD
+"""
+
+from collections import defaultdict
+
+
+class Dhatupatha(object):
+
+    def __init__(self, filename=None):
+        self.all_gana = defaultdict(set)
+
+        if filename is not None:
+            self.init(filename)
+
+    def init(self, filename):
+        """
+        :param filename: path to the Dhatupatha file
+        """
+        with open(filename) as f:
+            for line in f:
+                gana, number, dhatu = line.strip().split(',')
+                self.all_gana[dhatu].add(gana)
+
+    def gana(self, dhatu):
+        return self.all_gana[dhatu.svara]
+
+
+DHATUPATHA = Dhatupatha()
